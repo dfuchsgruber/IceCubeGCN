89b444a347ebdac176f6663bf584fe3e19e97cf7
Dominik Fuchsgruber <dfuchsgruber@submit-1.icecube.wisc.edu>
2019-11-02 07:59:43 -0500

    Fix submitting scripts

--- train.sh
+++ train.sh
@@ -4,5 +4,6 @@ cd ~/IceCubeGCN
 eval `/cvmfs/icecube.opensciencegrid.org/py3-v4.0.1/setup.sh`
 source ~/myenv3/bin/activate
 export HDF5_USE_FILE_LOCKING='FALSE'
+export ICECUBEGCNMEMMAPDIR='/data/user/dfuchsgruber/memmaps'
 python3 train.py settings/hd5.json
 
