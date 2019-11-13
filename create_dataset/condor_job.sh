cbfa0047982282fe60333260a02b0c5939d937fd
Wodka <domi.erdnuss2@gmx.de>
2019-11-13 23:53:14 +0100

    Fix paths

--- create_dataset/condor_job.sh
+++ create_dataset/condor_job.sh
@@ -5,5 +5,5 @@ cd ~/IceCubeGCN
 #source ~/myenv2/bin/activate
 export PYTONPATH=$PYTHONPATH:~/myenv2/lib/python2.7/site-packages/
 eval `/cvmfs/icecube.opensciencegrid.org/py2-v3.1.0/setup.sh`
-source /cvmfs/icecube.opensciencegrid.org/py2-v3/RHEL_7_x86_64/metaprojects/simulation/V06-00-00/env-shell.sh dataset/create.sh $1
+source /cvmfs/icecube.opensciencegrid.org/py2-v3/RHEL_7_x86_64/metaprojects/simulation/V06-00-00/env-shell.sh create_dataset/create.sh $1
 
